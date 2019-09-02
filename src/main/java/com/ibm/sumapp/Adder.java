a4d7d663b0946d8e472d48d047c4026bee5e9a4c
Andrew Moynahan <andrewmoynahan@ajm-mac.local>
2019-09-02 13:33:26 -0400

    SUMAPP-1 another comment changes

--- src/main/java/com/ibm/sumapp/Adder.java
+++ src/main/java/com/ibm/sumapp/Adder.java
@@ -6,7 +6,7 @@ public class Adder {
 	public int add( int a, int b ) {
 		int sum = a + b;
 		
-		// introduce an intermittent error
+		// introduce an intermittent error added a comment
  		if( a == 3 ) { 
  			sum = a*a + b*b;
  		} 
